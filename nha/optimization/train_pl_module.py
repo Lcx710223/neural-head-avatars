771c200be96b5a0f7aaae71c455669c48a0f9ae5
LCX@ARGOS <911239555@qq.com>
2025-06-23 15:35:13 +0800

    Update train_pl_module.py

--- nha/optimization/train_pl_module.py
+++ nha/optimization/train_pl_module.py
@@ -1,6 +1,8 @@
 ### LCX 20250621 大修改。把CKPTS的路径统一到:default_root_dir/checkpoints/last.ckpt里面去。default_root_dir，在ini文件里定义，目前我设置的是LCX-ME01。
 ### 保存、resume、评估都用LCX-ME01/checkpoints/last.ckpt。不再用Lightning自动拼出的version_x/checkpoints路径。只需保证dirpath和resume_from_checkpoint一致，且都用绝对路径或同一相对路径。
 ### LCX20250622修改了反序列化的代码，主要是加载CKPTS时的信任与安全问题。LCX20250623IMPORT JSON。
+### LCX20250623修改了：import json
+
 import time
 from collections import OrderedDict
 import json
